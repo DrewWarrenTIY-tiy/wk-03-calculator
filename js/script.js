a3a2b6c307e212342af194931db6f243f9f863dd
DrewWarrenTIY <drewwarrentiy@gmail.com>
2016-10-11 14:53:55 -0600

    feat(init) add four basic math functions
    Addition, Subtraction, Multiplication, Division

---
+++ js/script.js
@@ -0,0 +1,17 @@
+"use strict";
+
+function calcAdd(a, b) {
+  return a + b;
+}
+
+function calcSubstract(a, b) {
+  return a - b;
+}
+
+function calcMultiply(a, b) {
+  return a * b;
+}
+
+function calcDivide(a, b) {
+  return a / b;
+}
