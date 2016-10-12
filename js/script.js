6f4b905b44589eca642b40c5900e15a44d2d5292
DrewWarrenTIY <drewwarrentiy@gmail.com>
2016-10-11 18:06:58 -0600

    feat(init) Add function
    Create an array based on button clicks

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 "use strict";
 
+var calcInput = [];
+
 function calcAdd(a, b) {
   return a + b;
 }
@@ -24,3 +26,22 @@ function calcCalculate(a, b, cb) {
   return cb(a, b);
   console.log(cb(a,b));
 }
+
+function btnToArray(clicked_id) {
+  calcInput.push(clicked_id);
+  console.log(calcInput);
+}
+
+// function equalBtn() {
+//   if (calcInput[1] == '+') {
+//     calcInput[1] = calcAdd();
+//   }
+//   if (calcInput[1] == '-') {
+//     calcInput[1] = calcSubtract();
+//   }
+//   if (calcInput[1] == '*') {
+//     calcInput[1] = calcMultiply();
+//   }
+//
+//   calcCalculate(calcInput[0],calcInput[2], calcInput[1])
+// }
