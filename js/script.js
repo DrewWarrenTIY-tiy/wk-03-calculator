3855d1b251576951e97d148bbe0e84f4c79824c0
DrewWarrenTIY <drewwarrentiy@gmail.com>
2016-10-11 19:53:44 -0600

    feat(init) Make functions work;
    calc now displays answer on equals
    function names properly assigned from chosen operator

--- js/script.js
+++ js/script.js
@@ -26,13 +26,39 @@ function btnToArray(clicked_id) {
   calcInput.push(clicked_id);
   console.log(calcInput);
 }
-
+// NOT WORKING
 function equalButton() {
+  let a = parseFloat(calcInput[0]);
+  let b = parseFloat(calcInput[2]);
   if (calcInput[1] == '+') {
-    let cb = calcAdd();
-    let a = calcInput[0].parseInt;
-    let b = calcInput[2];
+    let cb = calcAdd;
     calcCalculate(a, b, cb);
+    document.querySelector('.answerBox').innerHTML = calcCalculate(a, b, cb);
+    console.log(calcCalculate(a, b, cb));
+  }
+  if (calcInput[1] == '-') {
+    let cb = calcSubtract;
+    calcCalculate(a, b, cb);
+    document.querySelector('.answerBox').innerHTML = calcCalculate(a, b, cb);
+    console.log(calcCalculate(a, b, cb));
+  }
+  if (calcInput[1] == '*') {
+    let cb = calcMultiply;
+    calcCalculate(a, b, cb);
+    document.querySelector('.answerBox').innerHTML = calcCalculate(a, b, cb);
+    console.log(calcCalculate(a, b, cb));
+  }
+  if (calcInput[1] == '/') {
+    let cb = calcDivide;
+    calcCalculate(a, b, cb);
+    document.querySelector('.answerBox').innerHTML = calcCalculate(a, b, cb);
+    console.log(calcCalculate(a, b, cb));
+  }
+  if (calcInput[1] == '%') {
+    let cb = calcRemainder;
+    calcCalculate(a, b, cb);
+    document.querySelector('.answerBox').innerHTML = calcCalculate(a, b, cb);
+    console.log(calcCalculate(a, b, cb));
   }
   console.log(calcInput);
 }
